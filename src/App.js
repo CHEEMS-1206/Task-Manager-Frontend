28a0754767919aae18bb8efc871e996bbe4e4a40
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-14 11:54:19 +0530

    Defined the routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>HOME</div>} />
+        <Route path="/logout" element={<div>LOGOUT</div>} />
+        <Route path="/login" element={<div>LOGIN</div>} />
+        <Route path="/register" element={<div>REGISTER</div>} />
+        <Route path="/all-tasks" element={<div>ALL-TASKS</div>} />
+        <Route path="/add-new-task" element={<div>ADD-NEW-TASK</div>} />
+        <Route path="/update-task" element={<div>UPDATE-TASK</div>} />
+        <Route path="/delete-task" element={<div>DELETE-TASK</div>} />
+        <Route path="my-tasks-analytics" element={<div>MY-TASK-ANALYTICS</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
