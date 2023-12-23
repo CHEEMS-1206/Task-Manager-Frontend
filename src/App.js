97785152463c5f423a69bd7d00603291d5f91c68
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-23 12:43:14 +0530

    updated styles of navbar component and updated the app.js rendering of component as per based on whether user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+
+import Home from "./Pages/Home.js";
+import Login from "./Pages/Login.js";
 
 function App() {
+  const isLoggedIn = localStorage.getItem("token") ? true : false;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>HOME</div>} />
-        <Route path="/logout" element={<div>LOGOUT</div>} />
-        <Route path="/login" element={<div>LOGIN</div>} />
-        <Route path="/register" element={<div>REGISTER</div>} />
-        <Route path="/all-tasks" element={<div>ALL-TASKS</div>} />
-        <Route path="/add-new-task" element={<div>ADD-NEW-TASK</div>} />
-        <Route path="/update-task" element={<div>UPDATE-TASK</div>} />
-        <Route path="/delete-task" element={<div>DELETE-TASK</div>} />
-        <Route path="my-tasks-analytics" element={<div>MY-TASK-ANALYTICS</div>} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/logout"
+          element={isLoggedIn ? <div>LOGOUT</div> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={isLoggedIn ? <Home /> : <div>Register</div>}
+        />
+        <Route
+          path="/my-tasks"
+          element={isLoggedIn ? <div>My tasks</div> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/add-new-task"
+          element={
+            isLoggedIn ? <div>Add New Task</div> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/update-task"
+          element={
+            isLoggedIn ? <div>Update task</div> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/remove-task"
+          element={
+            isLoggedIn ? <div>Delete task</div> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/my-tasks-analytics"
+          element={
+            isLoggedIn ? (
+              <div>My tasks analytics</div>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/my-task:id"
+          element={
+            isLoggedIn ? <div>Task by id</div> : <Navigate to="/login" />
+          }
+        />
+        <Route path="*" element={isLoggedIn ? <Home/> : <Navigate to="/login" /> } />
       </Routes>
     </BrowserRouter>
   );
