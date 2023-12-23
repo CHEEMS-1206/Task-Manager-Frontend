be30dcc0f91281cf40c1f93449e7345679db38dd
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-23 12:36:41 +0530

    updated the navbar with logic of conditional rendering on basis of user logged in or not

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./componentStyle.css";
 
 function Navbar() {
+  const isLoggedIn = localStorage.getItem("token") ? true : false;
+
   return (
     <div className="navbar">
       <div className="title-holder">Task Manager</div>
@@ -12,23 +14,31 @@ function Navbar() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/all-tasks">All Taks</Link>
+          <Link to="/my-tasks">All Taks</Link>
         </li>
         <li>
           <Link to="/add-new-task">Add Tasks</Link>
         </li>
         <li>
-          <Link to="/my-task-analytics">Task Analytics</Link>
+          <Link to="/my-tasks-analytics">Task Analytics</Link>
         </li>
       </ul>
-      <ul>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      {isLoggedIn ? (
+        <ul>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
