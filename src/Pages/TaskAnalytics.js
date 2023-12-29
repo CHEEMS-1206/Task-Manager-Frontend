97461f03d5dc08e3890b099b4dc957a53bee2d58
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-29 13:46:43 +0530

    added task analytics page rendered all charts in accordion component

---
+++ src/Pages/TaskAnalytics.js
@@ -0,0 +1,209 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../Component/Navbar.js";
+
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+import LoaderSpinner from "../Component/LoaderSpineer.js";
+import Accordion from "../Component/Accordion.js";
+
+import { AgChartsReact } from "ag-charts-react";
+
+const TasksAnalytics = ({ onLogout }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [numericalData, setNumericalData] = useState([]);
+  const [tasksByWeek, setTaksByWeek] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  toastr.options = {
+    position: "bottom-right",
+    hideDuration: 300,
+    timeOut: 3000,
+  };
+
+  const fetchTasks = async () => {
+    try {
+      setIsLoading(true);
+      const token = localStorage.getItem("token");
+
+      const response = await fetch(`http://localhost:5001/api/analytics`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+
+        // getting counts for firts chart
+        const statusCounts = data.reduce((acc, task) => {
+          acc[task.taskStatus] = (acc[task.taskStatus] || 0) + 1;
+          return acc;
+        }, {});
+        const numericalData = [
+          statusCounts["Pending"] || 0,
+          statusCounts["Completed"] || 0,
+          statusCounts["Missed"] || 0,
+        ];
+        setNumericalData(numericalData);
+
+        // chart 2 data getting data for last 28 days in 4 quarters
+        const currentDate = new Date();
+        const last28Days = new Date(
+          currentDate.getTime() - 28 * 24 * 60 * 60 * 1000
+        ); // Get date 28 days ago
+
+        // Filter tasks within the last 28 days
+        const filteredTasks = data.filter((task) => {
+          const taskDate = new Date(task.taskCreatedAt);
+          return taskDate >= last28Days && taskDate <= currentDate;
+        });
+
+        // Sort filtered tasks by taskCreatedAt in ascending order
+        filteredTasks.sort(
+          (a, b) => new Date(a.taskCreatedAt) - new Date(b.taskCreatedAt)
+        );
+
+        // Divide tasks into four quarters (last 7 days each)
+        const quarters = [[], [], [], []];
+        const quarterSize = 7;
+
+        filteredTasks.forEach((task) => {
+          const taskDate = new Date(task.taskCreatedAt);
+          const daysDiff = Math.floor(
+            (currentDate - taskDate) / (24 * 60 * 60 * 1000)
+          );
+          const quarterIndex = Math.floor(daysDiff / quarterSize);
+
+          quarters[quarterIndex].push(task);
+        });
+
+        // Calculate counts for each type of task in each quarter
+        const quarterStats = quarters.map((quarter, index) => {
+          const pendingTasks = quarter.filter(
+            (task) => task.taskStatus === "Pending"
+          ).length;
+          const completedTasks = quarter.filter(
+            (task) => task.taskStatus === "Completed"
+          ).length;
+          const missedTasks = quarter.filter(
+            (task) => task.taskStatus === "Missed"
+          ).length;
+
+          return {
+            quarter: `Q${index + 1}`,
+            pendingTasks,
+            completedTasks,
+            missedTasks,
+          };
+        });
+
+        console.log(quarterStats);
+        setTaksByWeek(quarterStats);
+
+        setTimeout(() => toastr.success("All tasks fetched."), 300);
+      } else {
+        console.log("Failed To fetch tasks. 1");
+        setTimeout(() => toastr.error("Couldn't fetch tasks."), 300);
+      }
+    } catch (error) {
+      setTimeout(() => toastr.error("An error occured."), 300);
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const chartItems = [
+    {
+      title: "Numerical Comparision",
+      content: (
+        <AgChartsReact
+          options={{
+            title: {
+              text: "Tasks Composition",
+            },
+            data: [
+              { tag: "Pending", numbers: numericalData[0] },
+              { tag: "Completed", numbers: numericalData[1] },
+              { tag: "Missed", numbers: numericalData[2] },
+            ],
+            series: [
+              {
+                type: "pie",
+                angleKey: "numbers",
+                legendItemKey: "tag",
+              },
+            ],
+          }}
+        />
+      ),
+    },
+    {
+      title: "Tasks By Date",
+      content: (
+        <AgChartsReact
+          className="quarterChart"
+          options={{
+            title: {
+              text: "Tasks statuses in last 28 Days.",
+            },
+            subtitle: {
+              text: "Segragated in quarters.",
+            },
+            data: tasksByWeek,
+            series: [
+              {
+                type: "bar",
+                xKey: "quarter",
+                yKey: "pendingTasks",
+                yName: "Pending Tasks",
+                stacked: true,
+                normalizedTo: 100,
+              },
+              {
+                type: "bar",
+                stacked: true,
+                normalizedTo: 100,
+                xKey: "quarter",
+                yKey: "completedTasks",
+                yName: "Completed Tasks",
+              },
+              {
+                type: "bar",
+                stacked: true,
+                normalizedTo: 100,
+                xKey: "quarter",
+                yKey: "missedTasks",
+                yName: "Missed Tasks",
+              },
+            ],
+          }}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <div className="task-analytics-page">
+      <Navbar onLogout={onLogout} />
+      {isLoading ? (
+        <div className="all-charts-holder">
+          <LoaderSpinner />
+        </div>
+      ) : (
+        <div className="all-charts-holder">
+          <h1 className="all-charts-holder-header">Your Tasks analytics </h1>
+          <Accordion items={chartItems} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TasksAnalytics;
