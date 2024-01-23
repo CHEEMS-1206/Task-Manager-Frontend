ecc6000d46b3e732119ed12db6d0aad5e9e8b5de
PRIYANSHU SINGH <sentysingh456@gmail.com>
2024-01-23 13:38:10 +0530

    moved [isLoggedIn, setIsLoggedIn] a stage upwards, rendered app for every data refresh, defined functions for setting, deleting and getting cookies, defined function for validating cookies, defined function to be executed onLogouts, rendered every route with proper props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Register from "./Pages/Regsiter.js";
 
@@ -10,45 +10,158 @@ import UpdateTask from "./Pages/UpdateTask.js";
 import TasksAnalytics from "./Pages/TaskAnalytics.js";
 
 function AppWrapper() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [key, setKey] = useState(0); // State to control the key of App component
 
   const rerenderApp = () => {
     setKey((prevKey) => prevKey + 1); // Increment key to trigger a re-render
   };
 
-  return <App key={key} rerenderApp={rerenderApp} />;
+  return (
+    <App
+      key={key}
+      rerenderApp={rerenderApp}
+      isLoggedIn={isLoggedIn}
+      setIsLoggedIn={setIsLoggedIn}
+    />
+  );
 }
 
-function App({ rerenderApp }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+function App(props) {
+  const setTokenInCookie = (cookieName) => {
+    // Setting Cookie
+    const expirationDate = new Date(Date.now() + 10 * 60 * 1000); // disable after 10 mins
+    const expires = expirationDate.toUTCString();
+    document.cookie = `token=${cookieName}; path=/; sameSite=Strict; Secure; expires=${expires}`;
+  };
 
-  useEffect(() => {
-    setIsLoggedIn(!!localStorage.getItem("token"));
-  }, [localStorage.getItem("token")]);
+  const getTokenFromCookie = (cookieName) => {
+    // Getting cookie
+    const cookies = document.cookie.split(";"); // Split the cookies string into an array
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim(); // Trim any leading or trailing spaces
+      // Check if this is the cookie we're looking for
+      if (cookie.startsWith(cookieName + "=")) {
+        return cookie.substring(cookieName.length + 1); // Return the value of the cookie
+      }
+    }
+    return null;
+  };
 
-  console.log(isLoggedIn);
+  const deleteTokenFromCookie = (cookieName) => {
+    const expirationDate = new Date(Date.now());
+    const expires = expirationDate.toUTCString();
+    document.cookie = `token=${cookieName}; path=/; sameSite=Strict; Secure; expires=${expires}`;
+  }
+
+  const validateToken = async () => {
+    const token = getTokenFromCookie("token");
+    if (token !== "") {
+      try {
+        const response = await fetch("http://localhost:5001/api/verify-token", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.status === 200) {
+          props.setIsLoggedIn(true);
+          // console.log(props.isLoggedIn, token);
+          return true;
+        } else if (response.status === 401) {
+          // token expired login again
+          response.json().then((error) => {
+            console.log(error.message);
+          });
+          return false;
+        } else if (response.status === 403) {
+          // invalid token login again
+          response.json().then((error) => {
+            console.log(error.message);
+          });
+          return false;
+        } else if (response.status === 500) {
+          // server error login again
+          response.json().then((error) => {
+            console.log(error.message);
+          });
+          return false;
+        }
+      } catch (error) {
+        // clientside error
+        console.log(error.message);
+      }
+    }
+  };
+
+  const onLogout = () => {
+    deleteTokenFromCookie("token");
+    props.setIsLoggedIn(false);
+    props.rerenderApp();
+  };
+
+  const handleUnethicalDataAccess = () => {};
+
+  const tokenInCookie = getTokenFromCookie("token");
+  if (tokenInCookie && tokenInCookie !== "") {
+    validateToken(tokenInCookie);
+  }
+
   return (
     <BrowserRouter>
-      {isLoggedIn ? (
+      {props.isLoggedIn ? (
         <Routes>
           <Route
             path="/"
-            element={<Home rerenderApp={rerenderApp} isLoggedIn={isLoggedIn} />}
+            element={
+              <Home
+                rerenderApp={props.rerenderApp}
+                isLoggedIn={props.isLoggedIn}
+                onLogout={onLogout}
+              />
+            }
           />
-          <Route path="/logout" element={<div>LOGOUT</div>} />
-          <Route path="/login" element={<Navigate to="/" />} />
           <Route
             path="/my-tasks"
-            element={<AllTasks rerenderApp={rerenderApp} />}
+            element={
+              <AllTasks
+                rerenderApp={props.rerenderApp}
+                isLoggedIn={props.isLoggedIn}
+                getTokenFromCookie={getTokenFromCookie}
+                onLogout={onLogout}
+              />
+            }
           />
           <Route
             path="/add-new-task"
-            element={<AddNewTask rerenderApp={rerenderApp} />}
+            element={
+              <AddNewTask
+                rerenderApp={props.rerenderApp}
+                isLoggedIn={props.isLoggedIn}
+                getTokenFromCookie={getTokenFromCookie}
+                onLogout={onLogout}
+              />
+            }
           />
-          <Route path="/update-task/:taskId" element={<UpdateTask />} />
+          <Route
+            path="/update-task/:taskId"
+            element={
+              <UpdateTask
+                getTokenFromCookie={getTokenFromCookie}
+              />
+            }
+          />
           <Route
             path="/my-tasks-analytics"
-            element={<TasksAnalytics rerenderApp={rerenderApp} />}
+            element={
+              <TasksAnalytics
+                rerenderApp={props.rerenderApp}
+                isLoggedIn={props.isLoggedIn}
+                getTokenFromCookie={getTokenFromCookie}
+                onLogout={onLogout}
+              />
+            }
           />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
@@ -59,7 +172,12 @@ function App({ rerenderApp }) {
           <Route
             path="/login"
             element={
-              <Login rerenderApp={rerenderApp} setIsLoggedIn={setIsLoggedIn} />
+              <Login
+                rerenderApp={props.rerenderApp}
+                setIsLoggedIn={props.setIsLoggedIn}
+                setTokenInCookie={setTokenInCookie}
+                onLogout={onLogout}
+              />
             }
           />
           <Route path="*" element={<Navigate to="/" />} />
